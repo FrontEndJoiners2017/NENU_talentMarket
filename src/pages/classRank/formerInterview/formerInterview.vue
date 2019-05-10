865f5c6815cd9cab760762c95886cc6f68f85150
tyxzf <41528426+tyxzf@users.noreply.github.com>
2019-05-10 12:35:30 +0800

    统一样式，交互数据

--- src/pages/classRank/formerInterview/formerInterview.vue
+++ src/pages/classRank/formerInterview/formerInterview.vue
@@ -1,206 +1,191 @@
 <template>
-	<div id="former">
-		<el-container>
-			<el-main>
-				<div id="intercity">
-					<el-card id="box-card">
-  						<div slot="header" class="clearfix" id="cardtitle">
-    						<span>走访城市排名检索</span>
-    						<el-button size="small">导入</el-button>
-    						<el-button size="small">导出</el-button>
-  						</div>
-  						<div  class="text item" id="searchbox">
-  							<el-form :inline="true" :model="formInline" class="demo-form-inline">
-								<el-form-item label="" id="select1">
-								    <el-select size="small" v-model="formInline.category" placeholder="教育类">
-								        <el-option label="教育类" value="教育类"></el-option>
-								      	<el-option label="非教育类" value="非教育类"></el-option>
-								    </el-select>
-								</el-form-item>
-								<el-form-item>
-									<el-input id="fillin" size="small" placeholder="城市名称/省份" v-model="input10" clearable></el-input>
-								</el-form-item>
-								<el-form-item label="">
-								    <el-select size="small" v-model="formInline.rank" placeholder="A">
-								        <el-option label="A" value="一等"></el-option>
-								      	<el-option label="B" value="二等"></el-option>
-								      	<el-option label="C" value="三等"></el-option>
-								      	<el-option label="D" value="四等"></el-option>
-								      	<el-option label="E" value="五等"></el-option>
-								    </el-select>
-								</el-form-item>
-								<el-form-item>
-								    <el-button size="small" @click="onSubmit">检索</el-button>
-								</el-form-item>
-							</el-form>
-  						</div>
-					</el-card>
-				</div>
-				
-				<div id="for_inter_tab">
-					<el-table :data="tableData" style="width: 100%" id="eltable">
-      					<el-table-column prop="order" label="序号"></el-table-column>
-      					<el-table-column prop="type" label="类型"></el-table-column>
-      					<el-table-column prop="city" label="城市"></el-table-column>
-      					<el-table-column prop="province" label="省份"></el-table-column>
-      					<el-table-column prop="intercount" label="走访次数"></el-table-column>
-      					<el-table-column prop="weightanalysis" label="权重分析"></el-table-column>
-      					<el-table-column prop="detailedinfor" label="详细信息">
-      						<template slot-scope="scope">
-        						<el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-						    </template>
-      					</el-table-column>
-    				</el-table>
-				</div>
-				
-				<div class="block" id="page"><!--分页-->
-				    <el-pagination layout="prev, pager, next" :total="1000"></el-pagination>
-				</div>
-				
-			</el-main>
-		</el-container>
+    <div>
+        <!-- 工具条 -->
+        <el-card class="box-card">
+        	<el-form :model="searchBox">
+                <el-row>
+                    <el-col :span="5">
+                        <el-form-item>
+                            <span id="TopTitle">走访城市排名检索</span>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="3">
+                        <el-form-item>
+                    		<el-button type="primary" id="btn">导入</el-button>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="3">
+                        <el-form-item>
+							<el-button type="primary" id="btn1">导出</el-button>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+                <el-row>
+                	<el-col :span="6">
+                		<el-form-item>
+                			<el-select v-model="value8" filterable placeholder="教育类" class="Select">
+                       			<el-option
+		                            v-for="item in types"
+		                            :key="item.value"
+		                            :label="item.label"
+		                            :value="item.value">
+		                        </el-option>
+		                    </el-select>
+                        </el-form-item>
+                	</el-col>
+                	<el-col :span="6">
+                		<el-form-item>
+                			<el-select v-model="value8" filterable placeholder="城市名称/省份" class="Select">
+		                        <el-option
+		                            v-for="item in cities"
+		                            :key="item.value"
+		                            :label="item.label"
+		                            :value="item.value">
+		                        </el-option>
+		                    </el-select>
+                        </el-form-item>
+                	</el-col>
+                	<el-col :span="6">
+                		<el-form-item>
+                			<el-select v-model="value8" filterable placeholder="A" class="Select">
+		                        <el-option
+		                            v-for="item in weights"
+		                            :key="item.value"
+		                            :label="item.label"
+		                            :value="item.value">
+		                        </el-option>
+		                   </el-select>
+                        </el-form-item>
+                	</el-col>
+                	<el-col :span="5">
+                        <el-form-item>
+                            <el-button type="primary" icon="el-icon-search" @click="search()" plain>检索</el-button>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+            </el-form>
+        </el-card>
+    
+        <!-- 签约城市列表 -->
+        <el-card class="box-card">
+            <el-table id="Table_intercity" :data="interCity" highlight-current-row v-loading="listLoading" @selection-change="selsChange">
+                <el-table-column prop="index" label="序号">
+                </el-table-column>
+                <el-table-column prop="education_yon" label="类型" >
+                </el-table-column>
+                <el-table-column prop="city_name" label="城市" >
+                </el-table-column>
+                <el-table-column prop="province" label="省份" >
+                </el-table-column>
+                <el-table-column prop="city_visit" label="走访次数" >
+                </el-table-column>
+                <el-table-column prop="weight" label="权重分析" >
+                </el-table-column>
+                <!-- 查看详细信息 -->
+                <el-table-column prop="details" label="详细信息">
+                    <router-link to="cityDetails">
+                        <el-button type="text" size="small">查看</el-button>
+                    </router-link>
+                </el-table-column>
+            </el-table>
+            <!-- 签约城市列表分页 -->
+            <el-pagination layout="prev, pager, next" background :total="total" style="margin-top:20px;text-align:center">
+            </el-pagination>
+        </el-card>
+
+
+        
     </div>
 </template>
 
 <script>
-	export default {
-	  methods: {
+export default {
+    name:'visitCity',
+    methods: {
+        // 查看
         handleClick(row) {
-          console.log(row);
-        },
-        onSubmit() {
-          console.log('submit!');
-        }
+        console.log(row);
       },
-      
-    
-      data() {
+      //分页设置，保持传入分页的当前页，令定义的当前页=分页的当前页
+        // current_change(currentPage){
+        // this.currentPage = currentPage;
+        // },
+        // find(){
+        // }
+    },
+    data() {
         return {
-          formInline: {
-            category: '',
-            rank: ''
-          },
-          input10: '',
-          tableData: [{
-            order: '1',
-            type: '教育',
-            city:'长春市',
-            province:'吉林省',
-            intercount:'12',
-            weightanalysis:'A',
-          }, {
-            order: '2',
-            type: '教育',
-            city:'沈阳市',
-            province:'辽宁省',
-            intercount:'12',
-            weightanalysis:'A',
-          }],
-          options: [{
-	        value: '选项1',
-	        label: '教育类'
-	      }, {
-	        value: '选项2',
-	        label: '非教育类'
-	      }, {
-	        value1: '选项1',
-	        label: 'A'
-	      }, {
-	        value1: '选项2',
-	        label: 'B'
-	      }, {
-	        value1: '选项3',
-	        label: 'C'
-	      }, {
-	        value1: '选项4',
-	        label: 'D'
-	      }, {
-	        value1: '选项5',
-	        label: 'E'
-	      }],
-	      value: '',
-	      value1: ''
-	      
-        }
+    //loading加载
+        listLoading: false,
+    //分页设置，当前页数为1，每页装的元组为10，元组总数目初始为0
+        currentPage: 1,
+        pagesize: 10,
+        total: 0,
+    // 签约城市表格部分
+        interCity_users: [],
+        //   签约率表格部分
+        growRate: [],
+    //   select选择器中数据
+        types:[{
+            value: '1',
+            label: '教育类'
+        },{
+            value: '2',
+            label: '非教育类'
+        }]
+//      loading:true,
       }
-    }
-	
-	
+    },
+    // 获取数据后渲染
+    created(){
+        this.$ajax({
+            method:"get",
+            url:"http://localhost:8081/testBoot/listAll",
+            dataType:"json",
+            // 跨域
+            crossDomain:true,
+            // 保证每次请求得到的数据都是最新的而不是缓存的数据
+            cache:false,
+        }).then(resolve => {
+            for(let index=0;index<resolve.data;index++){
+				this.tableData[i] = JSON.stringify(resolve.data[i]);
+			}
+			this.interCity = resolve.data;
+				
+//			this.tableData = JSON.stringify(resolve.data);
+			console.log(resolve)
+			//获取数组长度并赋值给total
+			//this.total = resolve.data.report.length;
+			console.log(resolve.data);
+        },reject => {
+            console.log("失败",reject);
+        })
+    },
+  };
 </script>
 
 <style scoped>
-	/*card部分*/	
-	#box-card{
-        padding: 0% 2%;
-        border: #cbcaca 1px solid;
-        border-radius: 10px;
-        margin-top: 1%;
-        margin-bottom: 25px;
-        
-	}
-	#intercity{
-		width: 100%;
-        margin: 0 auto;
-        margin-top: 1%;
-	}
-	#cardtitle{
-		border: 0;
-	}
-	#cardtitle  span{float: left;}
-	#select1{
-		float: left;
-	}
-	#fillin{
-		margin-top: 3px;
-		float: left;
-	}
-	.el-card{
-		box-shadow: 0;
-	}
-	.el-card__header{
-		padding: 18px 0px;
-		border-bottom: 0px;
-	}
-	.el-card__body {
-    padding: 0px;
-	}
-	
-	
-	
-	.text {
-	    font-size: 14px;
-	}
-	
-	.item {
-	    margin-bottom: 18px;
-	}
-	.clearfix:before,
-	.clearfix:after {
-	    display: table;
-	    content: "";
-	}
-	.clearfix:after {
-	    clear: both;
-	}
-	
-	.box-card {
-	    width: 480px;
-	}
-	
-	/*表格*/
-	#eltable{
-		border-radius: 10px;
-        width: 100%;
-        padding: 1% 2%;
-        border: #cbcaca 1px solid;
-        margin-top: 1%;
-	}
-	
-	
-	
-	/*分页*/
-	#page{
-		margin-top: 20px;
-		margin-bottom: 20px;
-	}
+/* h1标题，为了保证和检索按钮在一行 */
+h1{
+    display: inline-block;
+}
+/* 搜索框 */
+.search{
+    display:inline-block;
+    margin-left: 70%;
+    vertical-align:middle;
+}
+/* card设置 */
+.box-card{
+    width:100%;
+    border-radius: 10px;
+    margin-top: 10px;
+}
+#TopTitle {
+        position: relative;
+        text-align: left;
+        /* border: 1px solid red; */
+        font-size: 19px;
+        margin-bottom: 1.7%;
+    }
 </style>
